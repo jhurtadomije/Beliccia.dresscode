962298919d1de389b6c1ce3b987ae49287dd713e
Jhurtadomije <jhurtadomije@gmail.com>
2025-05-09 13:04:19 +0200

    centrado en moviles

--- beliccia/src/components/Hero.jsx
+++ beliccia/src/components/Hero.jsx
@@ -18,6 +18,7 @@ export default function Hero() {
         margin: 0,
         padding: 0,
         position: 'relative',
+        objectPosition: 'center',
         overflow: 'hidden',
       }}
     >
